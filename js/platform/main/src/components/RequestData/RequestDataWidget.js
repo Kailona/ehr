2ad90f9003694383501309ee0e1abb3f500e467c
Aysel Afsar <aysel.afsar@gmail.com>
2020-12-14 14:55:31 +0300

    #8 Add Request Data modal dialog

--- js/platform/main/src/components/RequestData/RequestDataWidget.js
+++ js/platform/main/src/components/RequestData/RequestDataWidget.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { DashboardWidget } from '@kailona/ui';
 import { Box } from '@material-ui/core';
+import RequestDataModal from './RequestDataModal';
 
 export default class RequestDataWidget extends Component {
-    onWidgetClick = () => {
-        // TODO: Request Data Workflow
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isRequestDataModalOpen: false,
+        };
+    }
+
+    openRequestDataModal = () => {
+        this.setState({
+            isRequestDataModalOpen: true,
+        });
+    };
+
+    closeRequestDataModal = () => {
+        this.setState({
+            isRequestDataModalOpen: false,
+        });
     };
 
     render() {
         return (
-            <Box>
-                <DashboardWidget
-                    onClick={this.onWidgetClick}
-                    icon="ArchiveOutlined"
-                    name={t('ehr', 'Request Data from Provider')}
-                />
-            </Box>
+            <>
+                <RequestDataModal isOpen={this.state.isRequestDataModalOpen} onClose={this.closeRequestDataModal} />
+                <Box>
+                    <DashboardWidget
+                        onClick={this.openRequestDataModal}
+                        icon="ArchiveOutlined"
+                        name={t('ehr', 'Request Data from Provider')}
+                    />
+                </Box>
+            </>
         );
     }
 }
