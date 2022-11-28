0f43cbcac7ad1a76157ada4850ea032d693b150d
Onur Doğan <56091646+onur-dogan@users.noreply.github.com>
2022-11-28 04:40:03 +0300

    #115 Extend Physical Data with a chart done (#144)

---
+++ js/plugins/physicalData/src/timelineData/getTimelineBmi.js
@@ -0,0 +1,56 @@
+import PhysicalDataService from '../services/PhysicalDataService';
+
+export default async function getTimelineBmi(startDate, endDate) {
+    const physicalDataService = new PhysicalDataService();
+
+    const params = [
+        {
+            date: `ge${moment(startDate)
+                .hour(0)
+                .minute(0)
+                .second(0)
+                .utc()
+                .toISOString()}`,
+        },
+        {
+            date: `le${moment(endDate)
+                .hour(23)
+                .minute(59)
+                .second(59)
+                .utc()
+                .toISOString()}`,
+        },
+        {
+            code: 'http://loinc.org|34565-2',
+            _include: 'Observation:has-member',
+        },
+        {
+            _count: 10000,
+        },
+    ];
+
+    const physicalDataItems = await physicalDataService.fetchData(params);
+
+    const timelineData = [];
+
+    physicalDataItems.forEach(physicalData => {
+        if (!physicalData.date || !physicalData.bmi) {
+            return;
+        }
+
+        timelineData.push({
+            date: physicalData.date,
+            value: physicalData.bmi,
+        });
+
+        return timelineData.map(data => {
+            if (data.value.split) {
+                data.value = parseInt(data.value.split(' ')[0], 10);
+            }
+
+            return data;
+        });
+    });
+
+    return timelineData;
+}
